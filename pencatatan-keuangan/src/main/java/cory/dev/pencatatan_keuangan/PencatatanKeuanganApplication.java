702c1ad6ec785acc0124404dbc9714667e549397
Nama Anda <cory@mail.com>
2025-11-05 19:36:37 +0700

    Spring init

---
+++ pencatatan-keuangan/src/main/java/cory/dev/pencatatan_keuangan/PencatatanKeuanganApplication.java
@@ -0,0 +1,13 @@
+package cory.dev.pencatatan_keuangan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PencatatanKeuanganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PencatatanKeuanganApplication.class, args);
+	}
+
+}
---
+++ pencatatan-keuangan/src/test/java/cory/dev/pencatatan_keuangan/PencatatanKeuanganApplicationTests.java
@@ -0,0 +1,13 @@
+package cory.dev.pencatatan_keuangan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PencatatanKeuanganApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
